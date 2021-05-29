334103dad7092d94e581973e161c13d3ad01a49d
[AndreaS] <solfa_90@hotmail.it>
2021-05-29 15:53:33 +0200

    create product component

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,32 @@
 // feature 1
+import React from "react";
 import "./index.css";
 import AppLayout from "../Components/AppLayout";
 import Header from "../Components/Header";
 import Content from "../Components/Content";
 import Footer from "../Components/Footer";
+import data from "../data.json";
 
-function App() {
-  return (
-    <AppLayout>
-      <Header />
-      <Content />
-      <Footer />
-    </AppLayout>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <AppLayout>
+        <Header />
+        <Content products={this.state.products} />
+        <Footer />
+      </AppLayout>
+    );
+  }
 }
 
 export default App;
