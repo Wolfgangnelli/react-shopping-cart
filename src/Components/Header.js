214842e2b628eea1604f3214ed3389836d0be29a
[AndreaS] <solfa_90@hotmail.it>
2021-06-03 16:47:10 +0200

    modify package.json and css Headers

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4">
-      <NavLink to="/">React Shopping Cart</NavLink>
-      <NavLink to="/admin">Admin</NavLink>
+    <header className="p-4">
+      <div className="flex items-center justify-between max-w-screen-2xl mx-auto">
+        <NavLink to="/">React Shopping Cart</NavLink>
+        <NavLink to="/admin">Admin</NavLink>
+      </div>
     </header>
   );
 };
