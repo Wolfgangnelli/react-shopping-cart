15f319ea7efd7e3c1352ed51d360a850b2f0e52f
[AndreaS] <solfa_90@hotmail.it>
2021-06-03 20:59:12 +0200

    fix bugs

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 //mi connetto a mongodb con questo entry-point
 mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
+  process.env.MONGODB_URL ||
+    "mongodb+srv://react-shopping-cart-test:KWpfsxSDc7icXQ1T@cluster0.zqklj.mongodb.net/react-shopping-cart-test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
